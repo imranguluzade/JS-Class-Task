4405475ff2032e24122c48ed37fa21a6e40e2cf9
imranguluzade <imranguliyev211@gmail.com>
2023-05-16 15:19:15 +0400

    TASK SOLVED

---
+++ script.js
@@ -0,0 +1,37 @@
+class Students {
+  constructor(student, points, finalPoint) {
+    this.student = student;
+    this.points = points;
+    this.finalPoint = finalPoint;
+  }
+
+  calculator() {
+    let sumOfPoints = 0;
+    let resultOfAverage;
+    for (let i = 0; i < this.points.length; i++) {
+      sumOfPoints += this.points[i];
+    }
+    resultOfAverage =
+      (sumOfPoints + this.finalPoint) / (this.points.length + 1);
+    return resultOfAverage;
+  }
+  showActivity() {
+    const resultOfAverage = this.calculator();
+    if (resultOfAverage > 70) {
+      return `Tələbənin adı:${this.student}; Qiymətləri ${this.points}; Final Qiyməti: ${this.finalPoint} Status: Keçib`;
+    } else {
+      return `Tələbənin adı:${this.student}; Qiymətləri ${this.points}; Final Qiyməti: ${this.finalPoint} Status: Kəsilib`;
+    }
+  }
+}
+
+const students = [
+  new Students("Imran", [80, 25, 90], 75),
+  new Students("Kenan", [70, 65, 70], 80),
+  new Students("Elman", [60, 75, 80], 20),
+  new Students("Nicat", [80, 85, 90], 75),
+];
+
+for (const student of students) {
+  console.log(student.showActivity());
+}
